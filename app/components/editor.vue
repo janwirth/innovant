23b36aadec82f00616fd82238055857b1b6c1f00
Jan Wirth <jottenwe@gmail.com>
2016-05-25 01:25:18 +0200

    Implement create / delete in editor

--- app/components/editor.vue
+++ app/components/editor.vue
@@ -2,6 +2,7 @@
 .Editor
   .Editor-view
     section(v-for='module in innovation.modules' class='Module--{{module.type}}').Module
+      button(v-on:click='removeModule(innovation, module)').Innovation-removeModule x
       .Module-content
         // render each content bit
         div(v-for='(type, value) in module.content' class='{{type}}')
@@ -32,10 +33,18 @@
             .Input-labelLow(v-medium='input.label')
             input(type='range').Input-element
             .Input-labelHigh(v-medium='input.labelHigh')
+            button(v-on:click='removeInput(module, input)').Module-removeInput x
 
           div(v-if='input.type == "textarea"' class='Input--{{input.type}}')
             .Input-label(v-medium='input.label')
             textarea.Input-element
+            button(v-on:click='removeInput(module, input)').Module-removeInput x
+
+        template(v-for='input in defaults.inputs')
+          button(v-on:click='addInput(module, input)').Module-addInput Add {{$key}} field
+
+    template(v-for='module in defaults.modules')
+      button(v-on:click='addModule(innovation, module)').Innovation-addModule Add {{$key}} module
 
   .Editor-toolbar
     h1(v-medium='innovation.name' mode='inline')
@@ -44,7 +53,9 @@
 
 
 <script lang="coffee">
+innovationDefaults = require '../resources/demoInnovation.coffee'
 DB = window.localStorageDB
+console.log innovationDefaults
 
 db = null
 
@@ -53,10 +64,28 @@ module.exports =
     # init DB with each load of component
     db =  DB 'innovant', localStorage
     currentVersion = db.queryAll('innovationVersions', {query: {ID: @$route.params.ID}})[0]
+
     data = 
+      defaults:
+        inputs: innovationDefaults.inputs
+        modules: innovationDefaults.modules
       innovation: currentVersion
       versionID: currentVersion.ID # hack: localStorageDB screws up the id on update...
     return data
+
+  methods:
+    addModule: (innovation, module) ->
+      innovation.modules.push module()
+
+    addInput: (module, input) ->
+      module.analytics.inputs.push input()
+
+    removeInput: (module, input) ->
+      module.analytics.inputs.$remove input
+
+    removeModule: (innovation, module) ->
+      innovation.modules.$remove module
+
 
   directives:
     medium: require '../directives/medium'
